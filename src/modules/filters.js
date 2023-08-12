821dc3e39dcb4028e220be4b1d00fed00c0f0736
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-08-12 17:10:51 +0300

    caegoryFilter

--- src/modules/filters.js
+++ src/modules/filters.js
@@ -1,5 +1,9 @@
 export const searchFilter = (goods, value) => {
 
-    return goods.filter(el => el.title.includes(value))
+    return goods.filter(el => el.title.toLowerCase().includes(value.toLowerCase()))
 }
 
+export const caegoryFilter = (goods, text) => {
+
+    return goods.filter(el => el.category === text)
+}
