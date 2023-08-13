8accc32bc63159e303cf47cd8ede4541f497838e
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-08-14 00:18:33 +0300

    funcFilter

--- src/modules/filters.js
+++ src/modules/filters.js
@@ -1,9 +1,19 @@
-export const searchFilter = (goods, value) => {
-
-    return goods.filter(el => el.title.toLowerCase().includes(value.toLowerCase()))
-}
-
 export const caegoryFilter = (goods, text) => {
 
     return goods.filter(el => el.category === text)
+}
+
+export const funcFilter = (goods, minValue, maxValue, checkValue, searchValue) => {
+
+    return goods.filter((goodsItem) => {
+        const isMin = minValue.trim() ? goodsItem.price >= parseInt(minValue.trim()) : true;
+        const isMax = maxValue.trim() ? goodsItem.price <= parseInt(maxValue.trim()) : true;
+        const isSale = checkValue ? goodsItem.sale : true;
+        console.log(searchValue);
+        return isMin &&
+            isMax &&
+            isSale &&
+            goodsItem.title.toLowerCase().includes(searchValue.toLowerCase())
+    })
+
 }
--- src/modules/search.js
+++ src/modules/search.js
@@ -1,19 +1,41 @@
 import getData from "./getData";
 import renderGoods from "./renderGoods";
-import { searchFilter } from "./filters";
+import { funcFilter } from "./filters";
 import { debounce } from "./helpers";
 
 const search = () => {
     const searchInput = document.querySelector('.search-wrapper_input');
+    const minInp = document.getElementById('min');
+    const maxInp = document.getElementById('max');
+    const checkInp = document.getElementById('discount-checkbox');
+    const checkMark = document.querySelector('.filter-check_checkmark')
+    // checked
 
-    const debounceSearch = debounce((event) => {
+
+    const debounceFunc = debounce((min = '', max = '', checkValue = false, searchValue = '') => {
         getData().then((data) => {
-            renderGoods(searchFilter(data, event.target.value))
+            renderGoods(funcFilter(data, min, max, checkValue, searchValue))
         })
     }, 300)
 
+    searchInput.addEventListener('input', () => {
+        debounceFunc(minInp.value, maxInp.value, checkInp.value, searchInput.value)
+    });
 
-    searchInput.addEventListener('input', debounceSearch)
+    minInp.addEventListener('input', (ev) => {
+        debounceFunc(minInp.value, maxInp.value, checkInp.value, searchInput.value)
+    })
+    maxInp.addEventListener('input', (ev) => {
+        debounceFunc(minInp.value, maxInp.value, checkInp.checked, searchInput.value)
+    })
+    checkInp.addEventListener('input', (ev) => {
+        if (checkInp.checked) {
+            checkMark.classList.add('checked')
+        } else {
+            checkMark.classList.remove('checked')
+        };
+        debounceFunc(minInp.value, maxInp.value, checkInp.checked, searchInput.value)
+    })
 }
 
 export default search
