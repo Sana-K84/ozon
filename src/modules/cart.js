d7d7c70a65b49e7e19b3cd60f2645786d2a269fe
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-08-15 22:45:23 +0300

    cart localStorage

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -2,7 +2,7 @@ const cart = () => {
     const cartBtn = document.getElementById('cart');
     const cartModal = document.querySelector('.cart');
     const cartClose = cartModal.querySelector('.cart-close');
-
+    const goodsWrapper = document.querySelector('.goods');
 
 
     cartBtn.addEventListener('click', () => {
@@ -11,6 +11,22 @@ const cart = () => {
 
     cartClose.addEventListener('click', () => {
         cartModal.style.display = 'none'
+    })
+
+    goodsWrapper.addEventListener('click', (ev) => {
+        if (ev.target.classList.contains('btn-primary')) {
+            const card = ev.target.closest('.card');
+            const key = card.dataset.key;
+            const goods = JSON.parse(localStorage.getItem('goods'))
+            const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+            const goodItem = goods.find(el => {
+                return el.id == key
+            })
+
+            cart.push(goodItem)
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+        }
     })
 }
 export default cart
